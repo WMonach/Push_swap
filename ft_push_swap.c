70075dfe4b1285040ec8d64e02de81a0a32a149c
william monachon <mimimonach@gmail.com>
2022-03-11 19:22:07 +0100

    no leak just have to norm main and  .h

--- ft_push_swap.c
+++ ft_push_swap.c
@@ -6,43 +6,11 @@
 /*   By: wmonacho <wmonacho@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 12:05:08 by wmonacho          #+#    #+#             */
-/*   Updated: 2022/03/11 14:36:59 by wmonacho         ###   ########lyon.fr   */
+/*   Updated: 2022/03/11 19:03:51 by wmonacho         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
-
-// int	main(int argc, char **argv)
-// {
-// 	t_list		*tab_a;
-// 	t_list		*tab_b;
-// 	int			*tmp;
-// 	char		**tab;
-// 	int			size;
-
-// 	size = argc;
-// 	tab = ft_check(&size, argv);
-// 	if (tab == NULL)
-// 		return (-1);
-// 	tmp = ft_index(size, tab);
-// 	if (size == 2)
-// 		return (ft_free_shorcut(tab, tmp));
-// 	if (ft_already_sort(tmp, size) == 1 || tmp == NULL)
-// 		return (ft_free_shorcut(tab, tmp));
-// 	tab_a = ft_list_a(size, tmp);
-// 	tab_b = ft_list_b();
-// 	if (tab_a == NULL)
-// 		return (ft_free_shorcut(tab, tmp));
-// 	if (size < 6)
-// 	{
-// 		ft_tri_under_5(&tab_a, &tab_b, size);
-// 		ft_free_shorcutlist(tab, tmp, tab_a);
-// 		return (0);
-// 	}
-// 	ft_setradix(tab_a, tab_b, size);
-// 	ft_free_shorcutlist(tab, tmp, tab_a);
-// 	return (0);
-// }
 
 int	main(int argc, char **argv)
 {
@@ -58,36 +26,68 @@ int	main(int argc, char **argv)
 		return (-1);
 	tmp = ft_index(size, tab);
 	if (size == 2)
-	{
-		ft_free_tab(tab);
-		free(tmp);
-		return (-1);
-	}
+		return (ft_free_shorcut(tab, tmp));
 	if (ft_already_sort(tmp, size) == 1 || tmp == NULL)
-	{
-		ft_free_tab(tab);
-		free(tmp);
-		return (-1);
-	}
+		return (ft_free_shorcut(tab, tmp));
 	tab_a = ft_list_a(size, tmp);
 	tab_b = ft_list_b();
 	if (tab_a == NULL)
-	{
-		ft_free_tab(tab);
-		free(tmp);
-		return (-1);
-	}
+		return (ft_free_shorcut(tab, tmp));
 	if (size < 6)
 	{
 		ft_tri_under_5(&tab_a, &tab_b, size);
-		ft_free_tab(tab);
-		free(tmp);
-		ft_free_list(tab_a);
+		ft_free_shorcutlist(tab, tmp, tab_a);
 		return (0);
 	}
 	ft_setradix(tab_a, tab_b, size);
-	ft_free_tab(tab);
-	free(tmp);
-	ft_free_list(tab_a);
+	ft_free_shorcutlist(tab, tmp, tab_a);
 	return (0);
 }
+
+// int	main(int argc, char **argv)
+// {
+// 	t_list		*tab_a;
+// 	t_list		*tab_b;
+// 	int			*tmp;
+// 	char		**tab;
+// 	int			size;
+
+// 	size = argc;
+// 	tab = ft_check(&size, argv);
+// 	if (tab == NULL)
+// 		return (-1);
+// 	tmp = ft_index(size, tab);
+// 	if (size == 2)
+// 	{
+// 		ft_free_tab(tab);
+// 		free(tmp);
+// 		return (-1);
+// 	}
+// 	if (ft_already_sort(tmp, size) == 1 || tmp == NULL)
+// 	{
+// 		ft_free_tab(tab);
+// 		free(tmp);
+// 		return (-1);
+// 	}
+// 	tab_a = ft_list_a(size, tmp);
+// 	tab_b = ft_list_b();
+// 	if (tab_a == NULL)
+// 	{
+// 		ft_free_tab(tab);
+// 		free(tmp);
+// 		return (-1);
+// 	}
+// 	if (size < 6)
+// 	{
+// 		ft_tri_under_5(&tab_a, &tab_b, size);
+// 		ft_free_tab(tab);
+// 		free(tmp);
+// 		ft_free_list(tab_a);
+// 		return (0);
+// 	}
+// 	ft_setradix(tab_a, tab_b, size);
+// 	ft_free_tab(tab);
+// 	free(tmp);
+// 	ft_free_list(tab_a);
+// 	return (0);
+// }
